c033492d7f649424936c76d82c84bdaef015b1b2
Anderson Viana <andersonp.viana.contato@gmail.com>
2024-06-17 12:28:26 -0300

    feat - builder created

--- src/main/java/org/anderson/curso_ada/Main.java
+++
@@ -1,7 +0,0 @@
-package org.anderson.curso_ada;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/org/anderson/curso_ada/creational/builder/model/Person.java
@@ -0,0 +1,70 @@
+package org.anderson.curso_ada.creational.builder.model;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String name;
+    private Integer age;
+    private String document;
+    private String email;
+    private LocalDate birthdata;
+
+    public Person(String name, LocalDate birthdata, String email, String document, int age) {
+        this.name = name;
+        this.birthdata = birthdata;
+        this.email = email;
+        this.document = document;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public LocalDate getBirthdata() {
+        return birthdata;
+    }
+
+    public void setBirthdata(LocalDate birthdata) {
+        this.birthdata = birthdata;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", document='" + document + '\'' +
+                ", email='" + email + '\'' +
+                ", birthdata=" + birthdata +
+                '}';
+    }
+}
