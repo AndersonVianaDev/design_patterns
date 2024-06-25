286d1f893467b6d8b61adb0b085577b089f9f05c
Anderson Viana <andersonp.viana.contato@gmail.com>
2024-06-25 16:59:35 -0300

    refactor - removing unnecessary methods

--- src/main/java/org/anderson/curso_ada/creational/builder/example2/builders/JustFriesBuilder.java
+++ src/main/java/org/anderson/curso_ada/creational/builder/example2/builders/JustFriesBuilder.java
@@ -3,27 +3,8 @@ package org.anderson.curso_ada.creational.builder.example2.builders;
 public class JustFriesBuilder extends FastFoodMealBuilder {
 
     @Override
-    public void buildDrink() {
-
-    }
-
-    @Override
-    public void buildMain() {
-
-    }
-
-    @Override
     public void buildSide() {
         meal.setSide("Large fries");
     }
-
-    @Override
-    public void buildDessert() {
-
-    }
-
-    @Override
-    public void buildGift() {
-
-    }
+    
 }
