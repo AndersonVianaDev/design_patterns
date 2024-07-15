30d343b5abcc171a3e47ce4cad6a3e7f0bc0a4c1
AndersonVianaDev <andersonp.viana.contato@gmail.com>
2024-07-15 14:44:54 -0300

    refactor: adding locale us

--- src/main/java/org/anderson/curso_ada/structural/bridge/exemple1/converter/CSVConverter.java
+++ src/main/java/org/anderson/curso_ada/structural/bridge/exemple1/converter/CSVConverter.java
@@ -2,10 +2,12 @@ package org.anderson.curso_ada.structural.bridge.exemple1.converter;
 
 import org.anderson.curso_ada.structural.bridge.exemple1.employee.Employee;
 
+import java.util.Locale;
+
 public class CSVConverter implements Converter{
     @Override
     public String getEmployeeFormated(Employee employee) {
-        return String.format("%s, %d, %.2f", employee.getName(),
+        return String.format(Locale.US,"%s, %d, %.2f", employee.getName(),
                 employee.getAge(), employee.getSalary());
     }
 }
